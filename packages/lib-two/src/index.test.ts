f5e2ab6b0eec3490133c2a6aeb60168553e0c2f2
deresegetachew <deresegetachew@gmail.com>
2025-10-18 22:51:51 +0200

    refactor: comment out unused test for greet function

--- packages/lib-two/src/index.test.ts
+++ packages/lib-two/src/index.test.ts
@@ -3,16 +3,15 @@ import { greet } from "./index";
 import assert from "node:assert";
 
 describe("lib-two tests", () => {
-  it("greet should return the correct greeting", () => {
-    // // Arrange
-    // const expected = "Lib-two says: Hello from lib-one!";
-
-    // // Act
-    // const result = greet();
-
-    // // Assert
-    // assert.strictEqual(result, expected);
-
-    assert.equal(true, true);
+  // it("greet should return the correct greeting", () => {
+  //   // // Arrange
+  //   const expected = "Lib-two says: Hello from lib-one!";
+  //   // Act
+  //   const result = greet();
+  //   // Assert
+  //   assert.strictEqual(result, expected);
+  // });
+  it("greet should be defined", () => {
+    assert.ok(greet);
   });
 });
