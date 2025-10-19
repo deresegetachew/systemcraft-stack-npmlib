ccdb71a76be11ecfeebb4d43e69fde4f664102db
deresegetachew <deresegetachew@gmail.com>
2025-10-19 11:09:40 +0200

    feat: enhance greet function to include a magic number calculation

--- packages/lib-two/src/index.ts
+++ packages/lib-two/src/index.ts
@@ -1,3 +1,10 @@
 import { hello } from "@systemcraft/lib-one";
 
-export const greet = () => `Lib-two says: ${hello()}`;
+export const greet = () => {
+  const result = sumTwoNumbers(1, 2);
+  return `Lib-two says: ${hello()}, your magic number is ${result}`;
+};
+
+const sumTwoNumbers = (a: number, b: number) => {
+  return a + b;
+};
