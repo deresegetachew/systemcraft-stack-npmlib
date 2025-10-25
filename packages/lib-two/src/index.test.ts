437255207164aa8161d4f3430277f2f2b1f0f08b
deresegetachew <deresegetachew@gmail.com>
2025-10-25 15:55:31 +0200

    refactor: comment out greet test and retain sample test

--- packages/lib-two/src/index.test.ts
+++ packages/lib-two/src/index.test.ts
@@ -3,15 +3,17 @@ import { greet } from "./index";
 import assert from "node:assert";
 
 describe("lib-two tests", () => {
-  it("greet should return the correct greeting", () => {
-    // // Arrange
-    const expected =
-      "Lib-two says: Hello from lib-one!, your magic number is 3";
-    // Act
-    const result = greet();
-    // Assert
-    assert.strictEqual(result, expected);
-  });
+  // it("greet should return the correct greeting", () => {
+  //   // // Arrange
+  //   const expected =
+  //     "Lib-two says: Hello from lib-one!, your magic number is 3";
+  //   // Act
+  //   const result = greet();
+  //   // Assert
+  //   assert.strictEqual(result, expected);
+  // });
 
-  it("should ");
+  it("sample test", () => {
+    assert.strictEqual(1 + 1, 2);
+  });
 });
